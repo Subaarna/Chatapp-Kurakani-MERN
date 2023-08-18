a10f137e623513ed2d28a12af951c8848274103e
leclipse <leclipse136@gmail.com>
2023-08-18 11:05:01 +0545

    Database connected

--- database/databaseConnection.js
+++ database/databaseConnection.js
@@ -1,26 +1,22 @@
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient } = require('mongodb');
 const uri = "mongodb+srv://chatapp:chatapp123@cluster0.yplaf6x.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+async function databaseInstance(){
+  try{
+    const client = await MongoClient.connect(uri, {useUnifiedTopology: true});
+    await client.db().command({ping:1});
+    console.log("Connected to MongoDB");
+    return client
+  }catch(error){
+    console.error("Error connecting to MongoDB", error);
+    throw new error;
   }
 }
-run().catch(console.dir);
+
+const clientPromise = databaseInstance();
+function openCollection(collectionName){
+  return clientPromise.then(client=> client.db("chatapp").collection(collectionName));
+}
+module.exports = openCollection;
--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require("express");
+const openCollection = require("./database/databaseConnection");
 //call the express function to create the app
 const app = express();
 
 // Use the express.json() middleware to parse JSON requests
 app.use(express.json());
-
+const collectionName= "chatapp";
+openCollection(collectionName)
+.then (collection =>{
+        // Now you have the collection object, and you can perform operations on it
+    // For example, you can query the collection
+    collection.findOne({}).then(result=>{
+        console.log("Result from the database:", result);
+    });
+})
+.catch(error=>{
+    console.error("Error:", error);
+})
 app.listen(8000, ()=>{
 console.log("server is running on port 8000");
 });
