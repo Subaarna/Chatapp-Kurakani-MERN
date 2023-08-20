1054aa3841ee017a7a763ad40ee8262f8b12b9c9
leclipse <leclipse136@gmail.com>
2023-08-20 14:14:16 +0545

    Models created

---
+++ controllers/userController.js
@@ -0,0 +1,15 @@
+async function createUser(req, res) {
+    try {
+      const userCollection = await openCollection("users");
+  
+      // Create a new User object from the request body
+      const user = new User(req.body.userName, req.body.email, req.body.password);
+  
+      // Check if the email already exists
+      const existingUser = await userCollection.findOne({ email: user.email });
+      if (existingUser) {
+        return res
+          .status(409)
+          .json({ error: "User with this email already exists" });
+      }
+  
