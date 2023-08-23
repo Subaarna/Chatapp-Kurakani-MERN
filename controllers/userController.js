af95a1f4b05bbf8cbf80e6c1e6e36a8b5b956b5d
leclipse <leclipse136@gmail.com>
2023-08-23 16:12:08 +0545

    small changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,16 +1,4 @@
+const openCollection = require("../database/databaseConnection");
+const User = require("../models/userModels");
 
-async function createUser(req, res) {
-    try {
-      const userCollection = await openCollection("users");
-  
-      // Create a new User object from the request body
-      const user = new User(req.body.userName, req.body.email, req.body.password);
-  
-      // Check if the email already exists
-      const existingUser = await userCollection.findOne({ email: user.email });
-      if (existingUser) {
-        return res
-          .status(409)
-          .json({ error: "User with this email already exists" });
-      }
-  
+
