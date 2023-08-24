643ddc1d1fcc32ab91e0e75c37250d8e1cd60149
leclipse <leclipse136@gmail.com>
2023-08-24 10:10:41 +0545

    bug fixes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,11 @@
 const openCollection = require("../database/databaseConnection");
 const User = require("../models/userModels");
 
+async function createUser(req, res){
+   
+        const userCollection = await openCollection("users");
+
+        const user = new User(req.body.userName, req.body.email, req.body.password);;
+    
+}
 
