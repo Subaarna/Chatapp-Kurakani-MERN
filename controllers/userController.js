a82addf32f0cb2210b5101a7cd8077d7109e4af4
leclipse <leclipse136@gmail.com>
2023-08-29 19:28:10 +0545

    user signup  added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,18 @@
 const openCollection = require("../database/databaseConnection");
 const User = require("../models/userModels");
+const bcrypt = require("bcrypt");
 
 async function createUser(req, res){
    
         const userCollection = await openCollection("users");
 
-        const user = new User(req.body.userName, req.body.email, req.body.password);;
-    
+        const user = new User(req.body.userName, req.body.email, req.body.password);
+        const existingUser = await userCollection.findOne({email:user.email});
+        if(existingUser){
+                return res.json('User with this email already exists');
+        }
+    const hashedPassword = await bcrypt.hash(user.password, saltRounds);
+        user.password = hashedPassword;
+        const result = await userCollection.insertOne(user);
 }
 
