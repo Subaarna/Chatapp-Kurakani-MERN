f08c957fbb892ccc56722a7be770a8709006e9b4
leclipse <leclipse136@gmail.com>
2023-10-05 22:30:13 +0545

    User Login and delete User created

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,10 @@
 const openCollection = require("../database/databaseConnection");
 const User = require("../models/userModels");
 const bcrypt = require("bcrypt");
+const { userLogin } = require("../models/usermodels");
+const { GenerateAccessToken } = require("../helpers/authHelper");
+const { GenerateRefreshToken } = require("../helpers/authHelper");
+const { ObjectId } = require("mongodb");
 
 async function createUser(req, res){
    
@@ -16,3 +20,44 @@ async function createUser(req, res){
         const result = await userCollection.insertOne(user);
 }
 
+async function login(req, res){
+        try{
+                const userCollection = await openCollection("users");
+                const user= new userLogin(req.body.email, req.body.password);
+
+                const existingUser = await userCollection.findOne({email:user.email});
+                if(existingUser){
+                        return res.json({message: "User with this email already exists!"});
+
+                }
+                const isPasswordValid = await bcrypt.compare(
+                        user.password,
+                        existingUser.password
+                );
+        if(!isPasswordValid){
+                return res.json({message: "Incorrect password!"});
+        }
+        const accessToken = GenerateAccessToken(existingUser._id.toHexString());
+        const refreshToken = GenerateRefreshToken(existingUser._id.toHexString);
+
+        return res.json({
+                message: "User logged in successfully",
+                Id: existingUser._id,
+                email: existingUser.email,
+                accessToken: accessToken,
+                refreshToken: refreshToken,
+        });
+        
+        }catch(error){
+                console.error("Error during login:", error);
+                return res.json({error: "Internal server error"});
+        }
+}
+
+async function deleteUser(req, res){
+        const userCollection = await openCollection("users");
+        const {userId}= req.body;
+
+        const result = await userCollection.fondOneAndDelete({_id: new ObjectId(userId)
+        });
+}
